c2debe86a3571e19e4339b9e9595a2cbd3b50379
ASUS <ASUS@LAPTOP-2O9Q1EA2>
2023-06-07 14:30:43 +0530

    file addded

---
+++ testproject/src/tesr/dbCapabiltiesTest.java
@@ -0,0 +1,35 @@
+package tesr;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbCapabiltiesTest {
+
+	public static void main(String[] args) throws SQLException {
+		System.out.println("test");
+		try(Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "piyush1234");)
+		{
+			if(con!=null)
+			{
+				DatabaseMetaData dbmd=con.getMetaData();
+				if(dbmd!=null)
+				{
+					System.out.println("DB sq name"+dbmd.getDatabaseProductName());
+					System.out.println("DB sq version"+dbmd.getDatabaseProductVersion());
+					System.out.println("sql keyword"+dbmd.getSQLKeywords());
+					
+					
+				}
+			}
+			
+			
+		}
+		catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+	}
+
+}
